7cad6d5a24cdc32843de70f9cd6d548dd1f7bc00
ahmad.mohammadi <a.mohammadi1026@gmail.com>
2022-05-17 09:52:07 +0430

    modified q2

--- q2/q2.js
+++ q2/q2.js
@@ -20,14 +20,14 @@ let user = {
             nationalId,
             role
         }
-        window.localStorage.setItem('database', JSON.stringify(database));
+        localStorage.setItem('database', JSON.stringify(database));
 
     },
     Remove(id, role) {
-        let newdatabase = JSON.parse(window.localStorage.getItem('database'));
+        let newdatabase = JSON.parse(localStorage.getItem('database'));
         delete newdatabase[role][id];
-        window.localStorage.setItem('database', JSON.stringify(newdatabase));
-        newdatabase = JSON.parse(window.localStorage.getItem('database'));
+        localStorage.setItem('database', JSON.stringify(newdatabase));
+        newdatabase = JSON.parse(localStorage.getItem('database'));
         console.log(newdatabase);
     },
     Find() {
@@ -36,75 +36,262 @@ let user = {
 
 };
 
-user.Add("ahmad", "mohammadi", "654545", "student");
-user.Add("ahmad", "mohammadi", "654545", "student");
-user.Add("ahmad", "mohammadi", "654545", "student");
-user.Add("ahmad", "mohammadi", "654545", "student");
+
+function Student(fullName, birthday, age, address, nationalId, gender, phoneNumber, grade, studyYear) {
+    this._fullName = fullName;
+    this._birthday = birthday;
+    this._age = age;
+    this._address = address;
+    this._nationalId = nationalId;
+    this._gender = gender;
+    this._phoneNumber = phoneNumber;
+    this._grade = grade;
+    this._studyYear = studyYear;
+    Object.defineProperty(this, "fullName", {
+        set: function (value) {
+            this._fullName = value;
+            localStorage.setItem("student", JSON.stringify(this));
+        },
+        get: function () {
+            return `${this.fullName}`;
+        }
+    }),
+        Object.defineProperty(this, "birthday", {
+            set: function (value) {
+                this._birthday = value;
+            },
+            get: function () {
+                return `${this.birthday}`;
+            }
+        }),
+        Object.defineProperty(this, "age", {
+            set: function (value) {
+                this._age = value;
+            },
+            get: function () {
+                return `${this.age}`;
+            }
+        }),
+        Object.defineProperty(this, "address", {
+            set: function (value) {
+                this._address = value;
+            },
+            get: function () {
+                return `${this.address}`;
+            }
+        }),
+        Object.defineProperty(this, "gender", {
+            set: function (value) {
+                this._gender = value;
+            },
+            get: function () {
+                return `${this.gender}`;
+            }
+        }),
+        Object.defineProperty(this, "phoneNumber", {
+            set: function (value) {
+                this._phoneNumber = value;
+            },
+            get: function () {
+                return `${this.phoneNumber}`;
+            }
+        }),
+        Object.defineProperty(this, "grade", {
+            set: function (value) {
+                this._grade = value;
+            },
+            get: function () {
+                return `${this.grade}`;
+            }
+        }),
+        Object.defineProperty(this, "studyYear", {
+            set: function (value) {
+                this._studyYear = value;
+            },
+            get: function () {
+                return `${this.studyYear}`;
+            }
+        })
+}
 
 
-const Student = {
-
-    set fullName(value){
-        this.fullName = value;
-    },
-    get fullName(){
-        return this.fullName;
-    },
-
-    set age(value){
-        this.age = value;
-    },
-    get age(){
-        return this.age;
-    },
-
-    set address(value){
-        this.address = value;
-    },
-    get address(){
-        return this.address;
-    },
-
-    set nationalId(value){
-        this.nationalId = value;
-    },
-    get nationalId(){
-        return this.nationalId;
-    },
-
-    set gender(value){
-        this.gender = value;
-    },
-    get gender(){
-        return this.gender;
-    },
-
-    set phoneNumber(value){
-        this.phoneNumber = value;
-    },
-    get phoneNumber(){
-        return this.phoneNumber;
-    },
-
-    set grade(value){
-        this.grade = value;
-    },
-    get grade(){
-        return this.grade;
-    },
-
-    set studyYear(value){
-        this.studyYear = value;
-    },
-    get studyYear(){
-        return this.studyYear;
-    }
+let student = new Student("ahmad mohammdi", "1991/05/05");
 
 
 
+function Teacher(fullName, birthday, age, address, nationalId, gender, phoneNumber, Field, Email, Salary) {
+    this._fullName = fullName;
+    this._birthday = birthday;
+    this._age = age;
+    this._address = address;
+    this._nationalId = nationalId;
+    this._gender = gender;
+    this._phoneNumber = phoneNumber;
+    this._Field = Field;
+    this._Email = Email;
+    this._Salary = Salary;
+    Object.defineProperty(this, "fullName", {
+        set: function (value) {
+            this._fullName = value;
+            localStorage.setItem("teacher", JSON.stringify(this));
+        },
+        get: function () {
+            return `${this.fullName}`;
+        }
+    }),
+        Object.defineProperty(this, "birthday", {
+            set: function (value) {
+                this._birthday = value;
+            },
+            get: function () {
+                return `${this.birthday}`;
+            }
+        }),
+        Object.defineProperty(this, "age", {
+            set: function (value) {
+                this._age = value;
+            },
+            get: function () {
+                return `${this.age}`;
+            }
+        }),
+        Object.defineProperty(this, "address", {
+            set: function (value) {
+                this._address = value;
+            },
+            get: function () {
+                return `${this.address}`;
+            }
+        }),
+        Object.defineProperty(this, "gender", {
+            set: function (value) {
+                this._gender = value;
+            },
+            get: function () {
+                return `${this.gender}`;
+            }
+        }),
+        Object.defineProperty(this, "phoneNumber", {
+            set: function (value) {
+                this._phoneNumber = value;
+            },
+            get: function () {
+                return `${this.phoneNumber}`;
+            }
+        }),
+        Object.defineProperty(this, "Field", {
+            set: function (value) {
+                this._Field = value;
+            },
+            get: function () {
+                return `${this.Field}`;
+            }
+        }),
+        Object.defineProperty(this, "Email", {
+            set: function (value) {
+                this._Email = value;
+            },
+            get: function () {
+                return `${this.Email}`;
+            }
+        }),
+        Object.defineProperty(this, "Salary", {
+            set: function (value) {
+                this._Salary = value;
+            },
+            get: function () {
+                return `${this.Salary}`;
+            }
+        })
+
+}
 
 
 
 
+function Manager(fullName, birthday, age, address, nationalId, gender, phoneNumber, Email, Salary ,major) {
+    this._fullName = fullName;
+    this._birthday = birthday;
+    this._age = age;
+    this._address = address;
+    this._nationalId = nationalId;
+    this._gender = gender;
+    this._phoneNumber = phoneNumber;
+    this._Email = Email;
+    this._Salary = Salary;
+    this._major = major;
+    Object.defineProperty(this, "fullName", {
+        set: function (value) {
+            this._fullName = value;
+            localStorage.setItem("teacher", JSON.stringify(this));
+        },
+        get: function () {
+            return `${this.fullName}`;
+        }
+    }),
+        Object.defineProperty(this, "birthday", {
+            set: function (value) {
+                this._birthday = value;
+            },
+            get: function () {
+                return `${this.birthday}`;
+            }
+        }),
+        Object.defineProperty(this, "age", {
+            set: function (value) {
+                this._age = value;
+            },
+            get: function () {
+                return `${this.age}`;
+            }
+        }),
+        Object.defineProperty(this, "address", {
+            set: function (value) {
+                this._address = value;
+            },
+            get: function () {
+                return `${this.address}`;
+            }
+        }),
+        Object.defineProperty(this, "gender", {
+            set: function (value) {
+                this._gender = value;
+            },
+            get: function () {
+                return `${this.gender}`;
+            }
+        }),
+        Object.defineProperty(this, "phoneNumber", {
+            set: function (value) {
+                this._phoneNumber = value;
+            },
+            get: function () {
+                return `${this.phoneNumber}`;
+            }
+        }),
+        Object.defineProperty(this, "Email", {
+            set: function (value) {
+                this._Email = value;
+            },
+            get: function () {
+                return `${this.Email}`;
+            }
+        }),
+        Object.defineProperty(this, "Salary", {
+            set: function (value) {
+                this._Salary = value;
+            },
+            get: function () {
+                return `${this.Salary}`;
+            }
+        }),
+        Object.defineProperty(this, "major", {
+            set: function (value) {
+                this._major = value;
+            },
+            get: function () {
+                return `${this.major}`;
+            }
+        })
 
 }
